b784a011db7cd5d57c03143e9466f02cd0bec08a
Amit <amitaswal87@gmail.com>
2024-08-02 21:28:56 +0530

    Initial Commit

---
+++ AdjoeVideoSDK/AdjoeVideoSDK.h
@@ -0,0 +1,18 @@
+//
+//  AdjoeVideoSDK.h
+//  AdjoeVideoSDK
+//
+//  Created by apple  on 02/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdjoeVideoSDK.
+FOUNDATION_EXPORT double AdjoeVideoSDKVersionNumber;
+
+//! Project version string for AdjoeVideoSDK.
+FOUNDATION_EXPORT const unsigned char AdjoeVideoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjoeVideoSDK/PublicHeader.h>
+
+
